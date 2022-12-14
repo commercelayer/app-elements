02b6e8efbd8598c71cfd7b4a3f93c07fc1211b12
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-12-14 17:02:59 +0100

    docs: improve ListSimple stories

--- packages/storybook/src/stories/ListSimple.stories.tsx
+++ packages/storybook/src/stories/ListSimple.stories.tsx
@@ -3,6 +3,7 @@ import ListSimple from '#core-app-elements/lists/ListSimple'
 import Container from '#core-app-elements/atoms/Container'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { useState } from 'react'
+import PageHeading from '#core-app-elements/atoms/PageHeading'
 
 const setup: ComponentMeta<typeof ListSimple> = {
   title: 'Lists/ListSimple',
@@ -28,12 +29,12 @@ const Template: ComponentStory<typeof ListSimple> = (args) => (
 
 export const Default = Template.bind({})
 
-const TemplateFull: ComponentStory<typeof ListSimple> = (args) => {
+const TemplateWithPagination: ComponentStory<typeof ListSimple> = (args) => {
   const [page, setPage] = useState(1)
   return (
     <Container>
       <ListSimple
-        title='Full example'
+        title='My list'
         pagination={{
           recordsPerPage: 20,
           recordCount: 243,
@@ -45,19 +46,45 @@ const TemplateFull: ComponentStory<typeof ListSimple> = (args) => {
         <ListSimpleItem label='Customers' />
         <ListSimpleItem label='Orders' />
         <ListSimpleItem label='Prices' />
-        <ListSimpleItem
-          label='SKUs'
-          description='June 15, 2022 14:57'
-          icon={<MyIcon />}
-        />
         <ListSimpleItem label='SKU lists' />
         <ListSimpleItem label='Stock items' />
       </ListSimple>
     </Container>
   )
 }
+export const WithPagination = TemplateWithPagination.bind({})
 
-export const Full = TemplateFull.bind({})
+const TemplateFullPage: ComponentStory<typeof ListSimple> = (args) => {
+  const [page, setPage] = useState(1)
+  return (
+    <Container>
+      <PageHeading
+        title='Resources'
+        badgeLabel='TEST-DATA'
+        description='Some optional description'
+        onGoBack={() => undefined}
+      />
+      <ListSimple
+        title='My list'
+        pagination={{
+          recordsPerPage: 20,
+          recordCount: 243,
+          currentPage: page,
+          onChangePageRequest: (newPage) => setPage(newPage),
+          pageCount: 5
+        }}
+      >
+        <ListSimpleItem label='Customers' description='June 15, 2022 14:57' />
+        <ListSimpleItem label='Orders' description='June 15, 2022 14:57' />
+        <ListSimpleItem label='Prices' description='June 15, 2022 14:57' />
+        <ListSimpleItem label='SKUs' description='June 15, 2022 14:57' />
+        <ListSimpleItem label='SKU lists' description='June 15, 2022 14:57' />
+        <ListSimpleItem label='Stock items' description='June 15, 2022 14:57' />
+      </ListSimple>
+    </Container>
+  )
+}
+export const FullPage = TemplateFullPage.bind({})
 
 function MyIcon(): JSX.Element {
   return (
