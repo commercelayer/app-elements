310ecbac5bf9d7322bf8f00ef035d91e6e9a6520
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-01-10 18:48:17 +0100

    docs: add new story (InputReadonly)

---
+++ packages/storybook/src/stories/forms/InputReadonly.stories.tsx
@@ -0,0 +1,26 @@
+import InputReadonly from '#core-app-elements/forms/InputReadonly'
+import Container from '#core-app-elements/atoms/Container'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+const setup: ComponentMeta<typeof InputReadonly> = {
+  title: 'Forms/InputReadonly',
+  component: InputReadonly,
+  parameters: {
+    layout: 'padded'
+  }
+}
+export default setup
+
+const Template: ComponentStory<typeof InputReadonly> = (args) => {
+  return (
+    <Container minHeight={false}>
+      <InputReadonly {...args} value={args.value} />
+    </Container>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  label: 'Secret',
+  value: 'asd6as78d6asds'
+}
