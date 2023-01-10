5148cceccbeaf90d474a0ac1220f12014808e91f
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-01-10 09:28:39 +0100

    docs: add new stories (A, Legend, Pagination)

---
+++ packages/storybook/src/stories/atoms/A.stories.tsx
@@ -0,0 +1,19 @@
+import A from '#core-app-elements/atoms/A'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+const setup: ComponentMeta<typeof A> = {
+  title: 'Atoms/A',
+  component: A
+}
+export default setup
+
+const Template: ComponentStory<typeof A> = (args) => (
+  <A {...args}>I am a link</A>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  href: 'https://commercelayer.io/',
+  target: '_blank',
+  onClick: () => {}
+}
