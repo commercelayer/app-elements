ecc81b188c77eb095b94553845402a7483d4a0cc
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-01-25 17:19:51 +0100

    fix: expose ref in InputFile

--- packages/core-app-elements/src/ui/forms/InputFile.test.tsx
+++ packages/core-app-elements/src/ui/forms/InputFile.test.tsx
@@ -1,4 +1,4 @@
-import { InputFile } from './InputFile'
+import InputFile from './InputFile'
 import { render, RenderResult } from '@testing-library/react'
 import { testInvariant } from '#utils/tests'
 
