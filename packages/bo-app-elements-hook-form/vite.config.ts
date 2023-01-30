2f380609f1eb6b5ce23fb4f3cdb860484433e135
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-01-30 16:26:59 +0100

    chore(hook-form): set `@commercelayer/core-app-elements` as external dep

--- packages/bo-app-elements-hook-form/vite.config.ts
+++ packages/bo-app-elements-hook-form/vite.config.ts
@@ -25,7 +25,12 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-dom', 'react-hook-form'],
+      external: [
+        'react',
+        'react-dom',
+        'react-hook-form',
+        '@commercelayer/core-app-elements'
+      ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
