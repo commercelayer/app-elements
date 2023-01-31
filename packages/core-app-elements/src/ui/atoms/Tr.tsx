a49eeb2f31dd5f0e1c6dafaef200ccfbe47488c9
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-01-31 18:42:45 +0100

    chore: remove unused types and props from Tr

--- packages/core-app-elements/src/ui/atoms/Tr.tsx
+++ packages/core-app-elements/src/ui/atoms/Tr.tsx
@@ -1,25 +1,9 @@
-type TableRowVariant = 'tbody' | 'thead'
-
 interface Props extends React.HTMLAttributes<HTMLElement> {
-  variant?: TableRowVariant
   children?: React.ReactNode
 }
 
-const tableRowVariantCss: Record<TableRowVariant, string> = {
-  tbody: '',
-  thead: ''
-}
-
-export function Tr({
-  variant = 'tbody',
-  children,
-  ...rest
-}: Props): JSX.Element {
-  return (
-    <tr className={tableRowVariantCss[variant]} {...rest}>
-      {children}
-    </tr>
-  )
+export function Tr({ children, ...rest }: Props): JSX.Element {
+  return <tr {...rest}>{children}</tr>
 }
 
 export default Tr
