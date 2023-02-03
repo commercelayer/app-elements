483638844257190a5764a062f590ce18f699a0fd
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-02-03 11:56:48 +0100

    fix: remove duplicate configuration (path.alias)

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,26 +1,25 @@
+// @ts-check
+
 const { mergeConfig } = require("vite");
 const { resolve } = require("path");
+const tsconfigPaths = require('vite-tsconfig-paths')
 
+/** @type import('vite').UserConfig */
+const viteOverrides = {
+  plugins: [
+    tsconfigPaths.default({
+      projects: [
+        resolve(__dirname, "../../core-app-elements/tsconfig.json"),
+        resolve(__dirname, "../tsconfig.json")
+      ]
+    })
+  ]
+}
+
+/** @type import('@storybook/core-common').StorybookConfig */
 module.exports = {
   async viteFinal(config, { configType }) {
-    // return the customized config
-    return mergeConfig(config, {
-      // customize the Vite config here
-      resolve: {
-        alias: {
-          // the main package
-          "#core-app-elements": resolve(
-            __dirname,
-            "../../core-app-elements/src/ui"
-          ),
-          // reference for internal package resolution
-          "#ui": resolve(__dirname, "../../core-app-elements/src/ui"),
-          "#styles": resolve(__dirname, ".../../core-app-elements/src/styles"),
-          "#utils": resolve(__dirname, "../../core-app-elements/src/utils"),
-          "#helpers": resolve(__dirname, "../../core-app-elements/src/helpers"),
-        },
-      },
-    });
+    return mergeConfig(config, viteOverrides);
   },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
