d45aef5ea8d5fe477c5c01e219ad1db2674b03b8
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-02-07 18:18:14 +0100

    chore: add useClickAway hook to handle outside click

---
+++ packages/core-app-elements/src/hooks/useClickAway.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useRef, RefObject } from 'react'
+
+export const useClickAway = (
+  onClickAway?: () => void
+): RefObject<HTMLDivElement> | undefined => {
+  if (onClickAway != null) {
+    const ref = useRef<HTMLDivElement>(null)
+
+    const escapeListener = useCallback((event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClickAway()
+      }
+    }, [])
+
+    const clickListener = useCallback(
+      (event: MouseEvent) => {
+        if (
+          ref.current != null &&
+          !ref?.current.contains(event.target as Node)
+        ) {
+          onClickAway()
+        }
+      },
+      [ref.current]
+    )
+
+    useEffect(() => {
+      document.addEventListener('click', clickListener)
+      document.addEventListener('keyup', escapeListener)
+      return () => {
+        document.removeEventListener('click', clickListener)
+        document.removeEventListener('keyup', escapeListener)
+      }
+    }, [])
+
+    return ref
+  }
+}
