f29e9d6f2a4f8fc6d6989236a1059a5fd2499adf
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-02-22 11:53:17 +0100

    fix: change function arguments as object

--- packages/app-elements/src/helpers/date.test.ts
+++ packages/app-elements/src/helpers/date.test.ts
@@ -2,41 +2,59 @@ import { formatDate } from './date'
 
 describe('formatDate', () => {
   test('Should return a nice date string', () => {
-    expect(formatDate('2022-10-14T14:32:00.000Z')).toBe('Oct 14, 2022')
+    expect(
+      formatDate({
+        isoDate: '2022-10-14T14:32:00.000Z'
+      })
+    ).toBe('Oct 14, 2022')
   })
 
   test('Should accept a date with time', () => {
     // AM
-    expect(formatDate('2023-02-22T10:32:47.284Z', true)).toBe(
-      'Feb 22, 2023 · 10:32 AM'
-    )
+    expect(
+      formatDate({
+        isoDate: '2023-02-22T10:32:47.284Z',
+        includeTime: true
+      })
+    ).toBe('Feb 22, 2023 · 10:32 AM')
+
     // PM
-    expect(formatDate('2022-10-26T16:16:31.279Z', true)).toBe(
-      'Oct 26, 2022 · 4:16 PM'
-    )
+    expect(
+      formatDate({
+        isoDate: '2022-10-26T16:16:31.279Z',
+        includeTime: true
+      })
+    ).toBe('Oct 26, 2022 · 4:16 PM')
   })
 
   test('Should accept a specific timezone override', () => {
     // Sydney (day after)
     expect(
-      formatDate('2022-10-26T16:16:31.279Z', false, 'Australia/Sydney')
+      formatDate({
+        isoDate: '2022-10-26T16:16:31.279Z',
+        timezone: 'Australia/Sydney'
+      })
     ).toBe('Oct 27, 2022')
 
     // Rome
-    expect(formatDate('2022-10-26T16:16:31.279Z', true, 'Europe/Rome')).toBe(
-      'Oct 26, 2022 · 6:16 PM'
-    )
+    expect(
+      formatDate({
+        isoDate: '2022-10-26T16:16:31.279Z',
+        includeTime: true,
+        timezone: 'Europe/Rome'
+      })
+    ).toBe('Oct 26, 2022 · 6:16 PM')
   })
 
   test('Should not break when date format is wrong', () => {
-    expect(formatDate('20221T15:35:42.315Z')).toBe('N/A')
+    expect(formatDate({ isoDate: '20221T15:35:42.315Z' })).toBe('N/A')
   })
 
   test('Should not break if date is empty string', () => {
-    expect(formatDate('')).toBe('N/A')
+    expect(formatDate({ isoDate: '' })).toBe('N/A')
   })
 
   test('Should not break if no date is passed', () => {
-    expect(formatDate(undefined)).toBe('N/A')
+    expect(formatDate({ isoDate: undefined })).toBe('N/A')
   })
 })
--- packages/app-elements/src/helpers/date.ts
+++ packages/app-elements/src/helpers/date.ts
@@ -5,25 +5,30 @@ import { utcToZonedTime } from 'date-fns-tz'
  * Format the date as nice string
  * @param dateIsoString - to match iso string `created_at` or `updated_at` from the import object (or <any>_at). Example '2022-10-06T11:59:30.371Z'
  * @param includeTime - optional flag to also include time
- * @param timeZone - optional string to cast a specific timezone and override user's browser default (Eg: UTC)
+ * @param timezone - optional string to cast a specific timezone and override user's browser default (Eg: UTC)
  * @returns a nice string representation. Example: 'Jul 21, 2022' or 'Jul 21, 2022 · 1:16 PM' if includeTime
  */
-export function formatDate(
-  dateIsoString?: string,
-  includeTime?: boolean,
+export function formatDate({
+  isoDate,
+  includeTime = false,
   timezone = 'UTC'
-): string {
-  if (dateIsoString == null) {
+}: {
+  isoDate?: string
+  includeTime?: boolean
+  timezone?: string
+}): string {
+  if (isoDate == null) {
     return 'N/A'
   }
 
   try {
-    const date = new Date(dateIsoString)
+    const date = new Date(isoDate)
     const zonedDate = utcToZonedTime(date, timezone)
 
     // Check template patterns here: https://date-fns.org/v2.29.3/docs/format
-    const formatTemplate =
-      includeTime === true ? 'LLL dd, yyyy · h:mm b' : 'LLL dd, yyyy'
+    const formatTemplate = includeTime
+      ? 'LLL dd, yyyy · h:mm b'
+      : 'LLL dd, yyyy'
 
     return format(zonedDate, formatTemplate)
   } catch {
