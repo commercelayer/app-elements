12dacd92bf975fd3a1749b630feee532b6b817eb
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-03-01 09:37:08 +0100

    fix: replace `includeTime` boolean with a `format` union that can be discriminated to pass a `customTemplate`

--- packages/app-elements/src/helpers/date.test.ts
+++ packages/app-elements/src/helpers/date.test.ts
@@ -1,20 +1,11 @@
 import { formatDate } from './date'
 
 describe('formatDate', () => {
-  test('Should return a nice date string', () => {
-    expect(
-      formatDate({
-        isoDate: '2022-10-14T14:32:00.000Z'
-      })
-    ).toBe('Oct 14, 2022')
-  })
-
   test('Should accept a date with time', () => {
     // AM
     expect(
       formatDate({
-        isoDate: '2023-02-22T10:32:47.284Z',
-        includeTime: true
+        isoDate: '2023-02-22T10:32:47.284Z'
       })
     ).toBe('Feb 22, 2023 · 10:32 AM')
 
@@ -22,9 +13,27 @@ describe('formatDate', () => {
     expect(
       formatDate({
         isoDate: '2022-10-26T16:16:31.279Z',
-        includeTime: true
+        format: 'full'
       })
     ).toBe('Oct 26, 2022 · 4:16 PM')
+  })
+
+  test('Should return a nice date string', () => {
+    expect(
+      formatDate({
+        isoDate: '2022-10-14T14:32:00.000Z',
+        format: 'noTime'
+      })
+    ).toBe('Oct 14, 2022')
+  })
+
+  test('Should return a date without year', () => {
+    expect(
+      formatDate({
+        isoDate: '2022-10-14T14:32:00.000Z',
+        format: 'noYear'
+      })
+    ).toBe('Oct 14')
   })
 
   test('Should accept a specific timezone override', () => {
@@ -32,7 +41,8 @@ describe('formatDate', () => {
     expect(
       formatDate({
         isoDate: '2022-10-26T16:16:31.279Z',
-        timezone: 'Australia/Sydney'
+        timezone: 'Australia/Sydney',
+        format: 'noTime'
       })
     ).toBe('Oct 27, 2022')
 
@@ -40,7 +50,6 @@ describe('formatDate', () => {
     expect(
       formatDate({
         isoDate: '2022-10-26T16:16:31.279Z',
-        includeTime: true,
         timezone: 'Europe/Rome'
       })
     ).toBe('Oct 26, 2022 · 6:16 PM')
@@ -56,5 +65,16 @@ describe('formatDate', () => {
 
   test('Should not break if no date is passed', () => {
     expect(formatDate({ isoDate: undefined })).toBe('N/A')
+  })
+
+  test('Should accept custom template', () => {
+    expect(
+      formatDate({
+        isoDate: '2023-02-27T16:00:00.000Z',
+        timezone: 'Europe/Rome',
+        format: 'custom',
+        customTemplate: `do 'of' LLLL`
+      })
+    ).toBe('27th of February')
   })
 })
