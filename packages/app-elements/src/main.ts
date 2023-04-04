d642956e73500772f815b72b7d8dfbc11e697d79
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-04-04 17:32:16 +0200

    fix: add missing export for Timeline component

--- packages/app-elements/src/main.ts
+++ packages/app-elements/src/main.ts
@@ -51,6 +51,7 @@ export { PageError } from '#ui/composite/PageError'
 export { PageLayout } from '#ui/composite/PageLayout'
 export { PageSkeleton } from '#ui/composite/PageSkeleton'
 export { Report } from '#ui/composite/Report'
+export { Timeline } from '#ui/composite/Timeline'
 
 // Forms
 export { Input } from '#ui/forms/Input'
