1059b01ff5b477d7896ed5cc68dbbfdee8d198dc
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-05-19 09:13:27 +0200

    fix: check if field is in form before setting an API validation error message

--- packages/app-elements-hook-form/src/helpers/setApiFormErrors.test.ts
+++ packages/app-elements-hook-form/src/helpers/setApiFormErrors.test.ts
@@ -30,7 +30,8 @@ describe('setApiFormErrors', () => {
           }
         ]
       },
-      setError: MockedSetError
+      setError: MockedSetError,
+      formFields: ['first_name', 'phone']
     })
 
     expect(MockedSetError).toHaveBeenCalledWith(
@@ -80,6 +81,7 @@ describe('setApiFormErrors', () => {
         ]
       },
       setError: MockedSetError,
+      formFields: ['myCustomFieldName'],
       fieldMap: {
         first_name: 'myCustomFieldName'
       }
@@ -95,5 +97,54 @@ describe('setApiFormErrors', () => {
         shouldFocus: true
       }
     )
+  })
+
+  test('should treat as `root.apiError` a validation error for a field not found in formFields', () => {
+    const MockedSetError = vi.fn()
+
+    setApiFormErrors({
+      apiError: {
+        errors: [
+          {
+            code: 'VALIDATION_ERROR',
+            title: 'Required field',
+            detail: 'first_name - is required',
+            source: {
+              pointer: '/data/attributes/first_name'
+            }
+          },
+          {
+            code: 'VALIDATION_ERROR',
+            title: 'Required field',
+            detail: 'phone - is required',
+            source: {
+              pointer: '/data/attributes/phone'
+            }
+          }
+        ]
+      },
+      setError: MockedSetError,
+      formFields: ['first_name']
+    })
+
+    // validation error since first_name is in formFields
+    expect(MockedSetError).toHaveBeenCalledWith(
+      'first_name',
+      {
+        type: 'serverValidation',
+        message: 'Required field'
+      },
+      {
+        shouldFocus: true
+      }
+    )
+
+    // generic error since phone is not in formFields
+    expect(MockedSetError).toHaveBeenCalledWith('root.apiError', {
+      type: 'server',
+      message: 'phone - is required'
+    })
+
+    expect(MockedSetError).toHaveBeenCalledTimes(2)
   })
 })
