846f86f51256fe2adba795d9fb012bd623993788
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-08-09 12:22:13 +0200

    feat: add truck icon and show all the available icons in doc

--- packages/docs/src/stories/atoms/Icon.stories.tsx
+++ packages/docs/src/stories/atoms/Icon.stories.tsx
@@ -1,4 +1,6 @@
-import { Icon } from '#ui/atoms/Icon'
+import { CopyToClipboard } from '#ui/atoms/CopyToClipboard'
+import { Icon, iconNames } from '#ui/atoms/Icon'
+import { Text } from '#ui/atoms/Text'
 import { type Meta, type StoryFn } from '@storybook/react'
 
 const setup: Meta<typeof Icon> = {
@@ -15,6 +17,34 @@ const Template: StoryFn<typeof Icon> = (args) => <Icon {...args} />
 export const Default = Template.bind({})
 Default.args = {
   name: 'check'
+}
+
+export const AvailableIcons: StoryFn = () => (
+  <div
+    style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+      gridAutoFlow: 'row',
+      gap: '1rem'
+    }}
+  >
+    {iconNames.map((name) => (
+      <div key={name} className='flex flex-row gap-2 items-center align-middle'>
+        <Icon key={name} name={name} background='black' gap='small' />
+        <Text size='small' variant='info'>
+          {name}
+        </Text>
+        <CopyToClipboard showValue={false} value={name} />
+      </div>
+    ))}
+  </div>
+)
+AvailableIcons.parameters = {
+  docs: {
+    source: {
+      code: null
+    }
+  }
 }
 
 export const OrderWaiting = Template.bind({})
