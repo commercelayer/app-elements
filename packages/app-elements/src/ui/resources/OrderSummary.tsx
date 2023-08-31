5264b2ec3e07fffbbf4968bec95b6651ab41d1df
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-08-31 11:44:22 +0200

    fix: use hook-form in OrderSummary adjustment overlay

--- packages/app-elements/src/ui/resources/OrderSummary.tsx
+++ packages/app-elements/src/ui/resources/OrderSummary.tsx
@@ -2,17 +2,22 @@ import { useOverlay } from '#hooks/useOverlay'
 import { useCoreSdkProvider } from '#providers/CoreSdkProvider'
 import { Button } from '#ui/atoms/Button'
 import { withSkeletonTemplate } from '#ui/atoms/SkeletonTemplate'
+import { Spacer } from '#ui/atoms/Spacer'
 import { Text } from '#ui/atoms/Text'
 import {
   ActionButtons,
   type ActionButtonsProps
 } from '#ui/composite/ActionButtons'
 import { PageLayout } from '#ui/composite/PageLayout'
-import { InputCurrency } from '#ui/forms/InputCurrency'
 import { type CurrencyCode } from '#ui/forms/InputCurrency/currencies'
+import { HookedForm } from '#ui/hook-form/HookedForm'
+import { HookedInputCurrency } from '#ui/hook-form/HookedInputCurrency'
 import { FlexRow } from '#ui/internals/FlexRow'
 import type { Order } from '@commercelayer/sdk'
-import { Fragment, useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Fragment, useMemo, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { LineItems } from './LineItems'
 
 interface TotalRowProps {
@@ -114,71 +119,87 @@ function useAdjustTotalOverlay(order: Order, onChange?: () => void) {
   const [disabled, setDisabled] = useState<boolean>(false)
   const currencyCode = order.currency_code as Uppercase<CurrencyCode>
   const { sdkClient } = useCoreSdkProvider()
-  const [cents, setCents] = useState<number | null>(null)
   const { Overlay, open, close } = useOverlay()
+
+  const validationSchema = useMemo(
+    () =>
+      z.object({
+        adjustTotal: z
+          .number({
+            required_error: 'Please enter a negative or positive value.',
+            invalid_type_error: 'Please enter a negative or positive value.'
+          })
+          .superRefine((val, ctx) => {
+            if (val === 0) {
+              ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: 'Please enter a negative or positive value.'
+              })
+            }
+          })
+      }),
+    []
+  )
+  const formMethods = useForm({
+    defaultValues: {
+      adjustTotal: 0
+    },
+    resolver: zodResolver(validationSchema)
+  })
 
   return {
     close,
     open,
     Overlay: () => (
-      <Overlay
-        footer={
-          <Button
-            disabled={disabled}
-            fullWidth
-            type='button'
-            onClick={() => {
-              if (cents != null) {
-                setDisabled(true)
-                void sdkClient.adjustments
-                  .create({
-                    currency_code: currencyCode,
-                    amount_cents: cents,
-                    name: 'Manual adjustment'
-                  })
-                  .then(async (adjustment) => {
-                    return await sdkClient.line_items.create({
-                      order: sdkClient.orders.relationship(order.id),
-                      quantity: 1,
-                      item: adjustment
-                    })
-                  })
-                  .then(() => {
-                    setCents(null)
-                    onChange?.()
-                    close()
-                  })
-                  .finally(() => {
-                    setDisabled(false)
-                  })
-              }
-            }}
-          >
-            Apply
-          </Button>
-        }
-      >
-        <PageLayout
-          title='Adjust total'
-          onGoBack={() => {
-            close()
+      <Overlay>
+        <HookedForm
+          {...formMethods}
+          onSubmit={(values) => {
+            setDisabled(true)
+            sdkClient.adjustments
+              .create({
+                currency_code: currencyCode,
+                amount_cents: values.adjustTotal,
+                name: 'Manual adjustment'
+              })
+              .then(async (adjustment) => {
+                return await sdkClient.line_items.create({
+                  order: sdkClient.orders.relationship(order.id),
+                  quantity: 1,
+                  item: adjustment
+                })
+              })
+              .then(() => {
+                onChange?.()
+                formMethods.reset()
+                close()
+              })
+              .finally(() => {
+                setDisabled(false)
+              })
           }}
         >
-          <InputCurrency
-            isClearable
-            allowNegativeValue
-            disabled={disabled}
-            currencyCode={currencyCode}
-            label='Amount'
-            name='adjust-total'
-            cents={cents}
-            onChange={(cents) => {
-              if (cents != null) {
-                setCents(cents)
-              }
+          <PageLayout
+            title='Adjust total'
+            onGoBack={() => {
+              close()
             }}
-          />
-        </PageLayout>
+          >
+            <Spacer bottom='8'>
+              <HookedInputCurrency
+                isClearable
+                allowNegativeValue
+                disabled={disabled}
+                currencyCode={currencyCode}
+                label='Amount'
+                name='adjustTotal'
+              />
+            </Spacer>
+            <Button type='submit' fullWidth disabled={disabled}>
+              Apply
+            </Button>
+          </PageLayout>
+        </HookedForm>
       </Overlay>
     )
   }
