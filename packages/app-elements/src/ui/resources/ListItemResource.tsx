366374763d939cb466521b5543126f28c6d751ca
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-09-06 15:59:35 +0200

    chore: refactor ListItemResource to rely on transformers methods

--- packages/app-elements/src/ui/resources/ListItemResource.tsx
+++ packages/app-elements/src/ui/resources/ListItemResource.tsx
@@ -40,69 +40,27 @@ const getDisplayStatus = (resource: ListItemEnabledResource): DisplayStatus => {
   }
 }
 
-const ListItemIcon = (resource: ListItemEnabledResource): JSX.Element => {
-  const displayStatus = getDisplayStatus(resource)
-  return resource.type === 'orders' && resource.status === 'pending' ? (
-    <RadialProgress icon={displayStatus.icon} />
-  ) : (
-    <Icon
-      name={displayStatus.icon}
-      background={displayStatus.color}
-      gap='large'
-    />
-  )
-}
-
-const ListItemName = (resource: ListItemEnabledResource): JSX.Element => {
-  return (
-    <Text tag='div' weight='semibold' data-test-id='ListItemResource-number'>
-      {resource.type === 'orders'
-        ? `${resource.market?.name ?? ''} #${resource.number ?? ''}`
-        : resource.type === 'returns'
-        ? `${resource.order?.market?.name ?? ''} #${resource.number ?? ''}`
-        : ''}
-    </Text>
-  )
-}
-
-const ListItemDescription = (
+interface ListItemDescriptionConfig {
   resource: ListItemEnabledResource
-): JSX.Element => {
+  date?: string
+  additionalInfos?: JSX.Element
+}
+
+const getListItemDescription = ({
+  resource,
+  date = resource.updated_at,
+  additionalInfos
+}: ListItemDescriptionConfig): JSX.Element => {
   const displayStatus = getDisplayStatus(resource)
   const { user } = useTokenProvider()
-  const descriptionDate =
-    resource.type === 'orders'
-      ? resource.placed_at ?? resource.updated_at
-      : resource.updated_at
-  let additionalInfos = ''
-  if (resource.type === 'orders') {
-    const billingAddress = resource.billing_address
-    additionalInfos = useMemo<string>(
-      () =>
-        !isEmpty(billingAddress?.company)
-          ? billingAddress?.company ?? ''
-          : formatDisplayName(
-              billingAddress?.first_name ?? '',
-              billingAddress?.last_name ?? ''
-            ),
-      [billingAddress]
-    )
-  }
-
   return (
-    <Text
-      tag='div'
-      weight='medium'
-      size='small'
-      variant='info'
-      data-test-id='ListItemResource-content'
-    >
+    <>
       {formatDate({
         format: 'date',
-        isoDate: descriptionDate,
+        isoDate: date,
         timezone: user?.timezone
       })}
-      {!isEmpty(additionalInfos) ? ` · ${additionalInfos}` : ''}
+      {additionalInfos}
       {' · '}
       {displayStatus.task != null ? (
         <Text weight='semibold' size='small' variant='warning'>
@@ -111,63 +69,159 @@ const ListItemDescription = (
       ) : (
         displayStatus.label
       )}
-    </Text>
-  )
-}
-
-const ListItemRightContent = (
-  resource: ListItemEnabledResource
-): JSX.Element => {
-  return resource.type === 'orders' ? (
-    <>
-      <Text
-        tag='div'
-        weight='semibold'
-        data-test-id='ListItemOrder-total'
-        className='break-keep'
-        wrap='nowrap'
-      >
-        {resource.formatted_total_amount}
-      </Text>
-      <Text
-        tag='div'
-        weight='medium'
-        size='small'
-        variant='info'
-        data-test-id='ListItemOrder-payment'
-        wrap='nowrap'
-      >
-        {getOrderPaymentStatusName(resource.payment_status)}
-      </Text>
     </>
-  ) : (
-    <></>
   )
 }
 
-export const ListItemResource = withSkeletonTemplate<Props>(
-  ({ resource, tag = 'div', isLoading, delayMs, onClick, ...rest }) => {
-    return (
-      <ListItem
-        tag={tag}
-        icon={ListItemIcon(resource)}
-        alignItems='top'
-        data-test-id='ListItemResource'
-        onClick={onClick}
-        {...rest}
-      >
-        <div>
-          {ListItemName(resource)}
-          {ListItemDescription(resource)}
-        </div>
-        <div>
-          {tag === 'a' ? (
-            <Icon name='caretRight' />
-          ) : (
-            ListItemRightContent(resource)
-          )}
-        </div>
-      </ListItem>
+const getLisrItemIcon = (displayStatus: DisplayStatus): JSX.Element => (
+  <Icon
+    name={displayStatus.icon}
+    background={displayStatus.color}
+    gap='large'
+  />
+)
+
+interface ListItemResourceComponentProps {
+  name: string
+  description: JSX.Element | string
+  icon: JSX.Element
+  rightContent?: JSX.Element
+  showArrow?: boolean
+}
+type ListItemResourceConfig = Pick<Props, 'tag' | 'onClick'> &
+  ListItemResourceComponentProps
+
+const ListItemResourceComponent: React.FC<ListItemResourceConfig> = (props) => {
+  const {
+    name,
+    description,
+    icon,
+    rightContent,
+    showArrow = false,
+    tag = 'div',
+    onClick,
+    ...rest
+  } = props
+  return (
+    <ListItem
+      tag={tag}
+      icon={icon}
+      alignItems='top'
+      data-test-id='ListItemResource'
+      onClick={onClick}
+      {...rest}
+    >
+      <div>
+        <Text
+          tag='div'
+          weight='semibold'
+          data-test-id='ListItemResource-number'
+        >
+          {name}
+        </Text>
+        <Text
+          tag='div'
+          weight='medium'
+          size='small'
+          variant='info'
+          data-test-id='ListItemResource-content'
+        >
+          {description}
+        </Text>
+      </div>
+      <div>
+        {rightContent != null && !showArrow ? (
+          rightContent
+        ) : (
+          <Icon name='caretRight' />
+        )}
+      </div>
+    </ListItem>
+  )
+}
+
+function orderToProps(resource: Order): ListItemResourceComponentProps {
+  const displayStatus = getDisplayStatus(resource)
+
+  const billingAddress = resource.billing_address
+  const descriptionAdditionalInfos = useMemo<JSX.Element>(
+    () => (
+      <>
+        {!isEmpty(billingAddress?.company)
+          ? billingAddress?.company ?? ''
+          : formatDisplayName(
+              billingAddress?.first_name ?? '',
+              billingAddress?.last_name ?? ''
+            )}
+      </>
+    ),
+    [billingAddress]
+  )
+
+  return {
+    name: `${resource.market?.name ?? ''} #${resource.number ?? ''}`,
+    description: getListItemDescription({
+      resource,
+      date: resource.placed_at ?? resource.updated_at,
+      additionalInfos: descriptionAdditionalInfos
+    }),
+    icon:
+      resource.status === 'pending' ? (
+        <RadialProgress icon={displayStatus.icon} />
+      ) : (
+        getLisrItemIcon(displayStatus)
+      ),
+    rightContent: (
+      <>
+        <Text
+          tag='div'
+          weight='semibold'
+          data-test-id='ListItemOrder-total'
+          className='break-keep'
+          wrap='nowrap'
+        >
+          {resource.formatted_total_amount}
+        </Text>
+        <Text
+          tag='div'
+          weight='medium'
+          size='small'
+          variant='info'
+          data-test-id='ListItemOrder-payment'
+          wrap='nowrap'
+        >
+          {getOrderPaymentStatusName(resource.payment_status)}
+        </Text>
+      </>
     )
   }
-)
+}
+
+function returnToProps(resource: Return): ListItemResourceComponentProps {
+  const displayStatus = getDisplayStatus(resource)
+  return {
+    name: `${resource.order?.market?.name ?? ''} #${resource.number ?? ''}`,
+    description: '',
+    icon: getLisrItemIcon(displayStatus)
+  }
+}
+
+export const ListItemResource = withSkeletonTemplate<Props>((props) => {
+  const { resource, tag = 'div', isLoading, delayMs, onClick, ...rest } = props
+  const listItemProps = useMemo(() => {
+    switch (resource.type) {
+      case 'orders':
+        return orderToProps(resource)
+      case 'returns':
+        return returnToProps(resource)
+    }
+  }, [resource])
+  return (
+    <ListItemResourceComponent
+      {...listItemProps}
+      tag={tag}
+      onClick={onClick}
+      {...rest}
+    />
+  )
+})
