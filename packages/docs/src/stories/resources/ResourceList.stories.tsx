a7d34188dae2bc1d0b23b1f4afff6998f322c9bc
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-09-07 15:58:17 +0200

    docs: add story for infinite scrolling

--- packages/docs/src/stories/resources/ResourceList.stories.tsx
+++ packages/docs/src/stories/resources/ResourceList.stories.tsx
@@ -1,13 +1,12 @@
 import { CoreSdkProvider } from '#providers/CoreSdkProvider'
 import { MockTokenProvider as TokenProvider } from '#providers/TokenProvider/MockTokenProvider'
-import { AvatarLetter } from '#ui/atoms/AvatarLetter'
 import { BlockCode } from '#ui/atoms/BlockCode'
 import { Button } from '#ui/atoms/Button'
 import { SkeletonTemplate } from '#ui/atoms/SkeletonTemplate'
 import { Spacer } from '#ui/atoms/Spacer'
 import { Text } from '#ui/atoms/Text'
-import { ListItem } from '#ui/composite/ListItem'
 import { InputCheckboxGroup } from '#ui/forms/InputCheckboxGroup'
+import { ListItemOrder } from '#ui/resources/ListItemOrder'
 import { ResourceList } from '#ui/resources/ResourceList'
 import { type Meta, type StoryFn } from '@storybook/react'
 
@@ -25,30 +24,127 @@ const setup: Meta<typeof ResourceList> = {
 }
 export default setup
 
-const Template: StoryFn<typeof ResourceList> = (args) => {
+export const WithItem: StoryFn<typeof ResourceList> = () => {
   return (
     <TokenProvider kind='integration' appSlug='orders' devMode>
       <CoreSdkProvider>
-        <ResourceList {...args} type='markets' />
+        <ResourceList
+          title='Orders'
+          emptyState={<div>Empty</div>}
+          type='orders'
+          actionButton={<Button variant='link'>Add new</Button>}
+          Item={({ resource = { id: '' }, isLoading }) => {
+            return (
+              <SkeletonTemplate isLoading={isLoading}>
+                <ListItemOrder order={resource as any} />
+              </SkeletonTemplate>
+            )
+          }}
+        />
       </CoreSdkProvider>
     </TokenProvider>
   )
 }
 
-export const WithItem = Template.bind({})
-WithItem.args = {
-  type: 'markets',
-  title: 'Markets',
-  emptyState: <div>Empty</div>,
-  actionButton: <Button variant='link'>Add new</Button>,
-  Item: ({ resource = { id: '', name: '' }, isLoading }) => (
-    <SkeletonTemplate isLoading={isLoading}>
-      <ListItem tag='div' icon={<AvatarLetter text={resource.id} />}>
-        {/* @ts-expect-error when passing arguments in the story we cannot get the typed resource (eg. Market). This doesn't happen on normal component usage */}
-        {resource?.name}
-      </ListItem>
-    </SkeletonTemplate>
+export const AsInputCheckboxGroup: StoryFn<typeof ResourceList> = (args) => {
+  return (
+    <TokenProvider kind='integration' appSlug='orders' devMode>
+      <CoreSdkProvider>
+        <ResourceList type='markets' emptyState={<div>Empty</div>}>
+          {({ data, isLoading }) => {
+            return (
+              <div>
+                {isLoading || data?.list[0]?.id == null ? (
+                  <div>loading</div>
+                ) : (
+                  <InputCheckboxGroup
+                    title='Select items'
+                    options={data.list.map((market) => ({
+                      value: market.id,
+                      content: <div>{market.name}</div>,
+                      quantity: {
+                        max: 10,
+                        min: 1
+                      }
+                    }))}
+                    onChange={() => {}}
+                  />
+                )}
+              </div>
+            )
+          }}
+        </ResourceList>
+      </CoreSdkProvider>
+    </TokenProvider>
   )
+}
+
+export const WithFunctionAsChild = (): JSX.Element => {
+  return (
+    <TokenProvider kind='integration' appSlug='orders' devMode>
+      <CoreSdkProvider>
+        <ResourceList type='markets' emptyState={<div>Empty</div>}>
+          {({ data, isLoading }) => {
+            return (
+              <div>
+                <Spacer bottom='4'>
+                  <Text size='large'>Markets as json</Text>
+                </Spacer>
+                <Spacer bottom='4'>
+                  <Text>
+                    You can use the following <Pre>`data`</Pre> received as
+                    arguments, to build own render. A fetch request to get data
+                    from the next page, will be triggered once the bottom of the
+                    component is visible and <Pre>`data`</Pre> object will be
+                    updated with new content.
+                  </Text>
+                </Spacer>
+                {isLoading || data?.list[0]?.id == null ? (
+                  <div>loading</div>
+                ) : (
+                  <BlockCode json={data} />
+                )}
+              </div>
+            )
+          }}
+        </ResourceList>
+      </CoreSdkProvider>
+    </TokenProvider>
+  )
+}
+
+/**
+ * By default, when the list has multiple pages, the component will show more results using an infinite scrolling.
+ */
+export const WithInfiniteScrolling: StoryFn<typeof ResourceList> = () => {
+  return (
+    <TokenProvider kind='integration' appSlug='orders' devMode>
+      <CoreSdkProvider>
+        <ResourceList
+          title='Orders'
+          type='orders'
+          emptyState={<div>Empty</div>}
+          Item={({ resource = { id: '' }, isLoading }) => {
+            return (
+              <SkeletonTemplate isLoading={isLoading}>
+                <ListItemOrder order={resource as any} />
+              </SkeletonTemplate>
+            )
+          }}
+          query={{
+            pageSize: 10
+          }}
+        />
+      </CoreSdkProvider>
+    </TokenProvider>
+  )
+}
+WithInfiniteScrolling.parameters = {
+  docs: {
+    source: {
+      code: null
+    }
+  }
 }
 
 const Pre: React.FC<{ children: string }> = ({ children }) => (
@@ -56,60 +152,3 @@ const Pre: React.FC<{ children: string }> = ({ children }) => (
     {children}
   </pre>
 )
-
-export const AsInputCheckboxGroup = Template.bind({})
-AsInputCheckboxGroup.args = {
-  type: 'markets',
-  emptyState: <div>Empty</div>,
-  children: ({ data, isLoading }) => {
-    return (
-      <div>
-        {isLoading || data?.list[0]?.id == null ? (
-          <div>loading</div>
-        ) : (
-          <InputCheckboxGroup
-            title='Select items'
-            options={data.list.map((market) => ({
-              value: market.id,
-              // @ts-expect-error when passing arguments in the story we cannot get the typed resource
-              content: <div>{market.name}</div>,
-              quantity: {
-                max: 10,
-                min: 1
-              }
-            }))}
-            onChange={() => {}}
-          />
-        )}
-      </div>
-    )
-  }
-}
-
-export const WithFunctionAsChild = Template.bind({})
-WithFunctionAsChild.args = {
-  type: 'markets',
-  emptyState: <div>Empty</div>,
-  children: ({ data, isLoading }) => {
-    return (
-      <div>
-        <Spacer bottom='4'>
-          <Text size='large'>Markets as json</Text>
-        </Spacer>
-        <Spacer bottom='4'>
-          <Text>
-            You can use the following <Pre>`data`</Pre> received as arguments,
-            to build own render. A fetch request to get data from the next page,
-            will be triggered once the bottom of the component is visible and{' '}
-            <Pre>`data`</Pre> object will be updated with new content.
-          </Text>
-        </Spacer>
-        {isLoading || data?.list[0]?.id == null ? (
-          <div>loading</div>
-        ) : (
-          <BlockCode json={data} />
-        )}
-      </div>
-    )
-  }
-}
