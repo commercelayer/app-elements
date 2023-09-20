5727e0a3fc9dab41b336fdc78af8ce25caf1c9c4
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-09-20 15:38:13 +0200

    fix: update indentation of unit test suite

--- packages/app-elements/src/ui/resources/ResourceLineItems.test.tsx
+++ packages/app-elements/src/ui/resources/ResourceLineItems.test.tsx
@@ -134,9 +134,7 @@ describe('ResourceLineItems', () => {
     expect(queryAllByText('White Water Bottle with Black Logo').length).toEqual(
       1
     )
-    expect(queryAllByText('Enamel Mug with Black Logo').length).toEqual(
-      1
-    )
+    expect(queryAllByText('Enamel Mug with Black Logo').length).toEqual(1)
     expect(queryAllByText('Gift card: €9,00').length).toEqual(0)
     expect(queryAllByText('Gift card: €100,00').length).toEqual(1)
   })
