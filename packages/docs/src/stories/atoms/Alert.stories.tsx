a7e8ca4f9e8a4e27ea35b26c4d6143a27ff77245
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-10-16 14:52:53 +0200

    feat: add new 'Alert' component

---
+++ packages/docs/src/stories/atoms/Alert.stories.tsx
@@ -0,0 +1,21 @@
+import { Alert } from '#ui/atoms/Alert'
+import { type Meta, type StoryFn } from '@storybook/react'
+
+const setup: Meta<typeof Alert> = {
+  title: 'Atoms/Alert',
+  component: Alert
+}
+export default setup
+
+const Template: StoryFn<typeof Alert> = (args) => (
+  <Alert {...args}>
+    The new total is $80,00, $3.00 more than the original total.
+    <br />
+    Adjust the total to make it equal or less.
+  </Alert>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  status: 'warning'
+}
