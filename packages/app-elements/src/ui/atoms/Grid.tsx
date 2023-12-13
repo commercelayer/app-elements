6edab7a6bf220d7961330bf4153291eb708eab53
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-12-13 16:55:08 +0100

    fix: add `alignItems` prop in `<Grid>` component

--- packages/app-elements/src/ui/atoms/Grid.tsx
+++ packages/app-elements/src/ui/atoms/Grid.tsx
@@ -3,20 +3,35 @@ import React, { type ReactNode } from 'react'
 
 export interface GridProps
   extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+  /**
+   * Grid items
+   */
   children: ReactNode
+  /**
+   * Number of columns
+   */
   columns: '1' | '2'
+  /**
+   * Items alignment
+   * @default start
+   */
+  alignItems?: 'center' | 'start' | 'end'
 }
 
 function Grid({
   children,
   columns,
   className,
+  alignItems = 'start',
   ...rest
 }: GridProps): JSX.Element {
   return (
     <div
       className={cn('grid grid-cols-1 gap-4', className, {
-        'sm:grid-cols-2': columns === '2'
+        'sm:grid-cols-2': columns === '2',
+        'items-center': alignItems === 'center',
+        'items-start': alignItems === 'start',
+        'items-end': alignItems === 'end'
       })}
       {...rest}
     >
