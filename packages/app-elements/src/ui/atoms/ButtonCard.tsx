aa3efc3701d83381ed07dc805a01e6aeacb0e6d4
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-01-12 18:43:43 +0100

    feat: add ButtonCard component

---
+++ packages/app-elements/src/ui/atoms/ButtonCard.tsx
@@ -0,0 +1,89 @@
+import cn from 'classnames'
+import { Icon, type IconProps } from './Icon'
+
+type ButtonCardBorderVariant = 'dashed'
+
+type ButtonCardPadding = 'none' | '4' | '6'
+
+const borderVariantCss: Record<ButtonCardBorderVariant, string> = {
+  dashed: 'border-dashed'
+}
+
+const paddingCss: Record<ButtonCardPadding, string> = {
+  none: 'p-0',
+  '4': 'p-4',
+  '6': 'p-6'
+}
+
+export interface ButtonCardProps
+  extends React.HTMLAttributes<HTMLButtonElement> {
+  onClick?: () => void
+  border?: ButtonCardBorderVariant
+  padding?: ButtonCardPadding
+  icon?: IconProps['name']
+  iconLabel?: string
+  fullWidth?: boolean
+  children?: React.ReactNode
+}
+
+function ButtonCard({
+  onClick,
+  border = 'dashed',
+  padding = '4',
+  icon,
+  iconLabel,
+  fullWidth,
+  children,
+  className,
+  ...rest
+}: ButtonCardProps): JSX.Element {
+  return (
+    <button
+      type='button'
+      data-testid='ButtonCard-main'
+      onClick={onClick}
+      className={cn(
+        'border border-gray-300 hover:border-primary hover:border-solid hover:ring-inset hover:ring-1 hover:ring-primary',
+        paddingCss[padding],
+        borderVariantCss[border],
+        children == null ? 'rounded' : 'rounded-md',
+        children == null && 'text-primary',
+        fullWidth === true && 'w-full',
+        className
+      )}
+      {...rest}
+    >
+      <div className='flex items-center justify-between'>
+        {children}
+        {(icon != null || iconLabel != null) && (
+          <div
+            className={cn(
+              'flex items-center gap-2 justify-center',
+              children == null ? 'w-full' : null
+            )}
+          >
+            {icon != null && (
+              <Icon
+                name={icon}
+                size={32}
+                className='shrink-0'
+                data-testid='ButtonCard-icon'
+              />
+            )}
+            {iconLabel != null && (
+              <span
+                className='inline-block font-bold'
+                data-testid='ButtonCard-iconLabel'
+              >
+                {iconLabel}
+              </span>
+            )}
+          </div>
+        )}
+      </div>
+    </button>
+  )
+}
+
+ButtonCard.displayName = 'ButtonCard'
+export { ButtonCard }
---
+++ packages/docs/src/stories/atoms/ButtonCard.stories.tsx
@@ -0,0 +1,56 @@
+import { ButtonCard } from '#ui/atoms/ButtonCard'
+import { Text } from '#ui/atoms/Text'
+import type { Meta, StoryFn } from '@storybook/react'
+
+const setup: Meta<typeof ButtonCard> = {
+  title: 'Atoms/ButtonCard',
+  parameters: {
+    layout: 'padded'
+  },
+  component: ButtonCard
+}
+
+export default setup
+
+const Template: StoryFn<typeof ButtonCard> = (args) => (
+  <div>
+    {/* <div className='container w-screen' style={{ width: '100vw' }}> */}
+    <ButtonCard {...args} />
+  </div>
+)
+
+export const Primary = Template.bind({})
+Primary.args = {
+  fullWidth: true,
+  icon: 'arrowDown',
+  iconLabel: 'Download',
+  onClick: () => {
+    alert('main clicked')
+  }
+}
+
+export const WithChildren = Template.bind({})
+WithChildren.args = {
+  fullWidth: true,
+  icon: 'arrowCircleUp',
+  padding: '6',
+  children: (
+    <Text align='left' variant='info'>
+      <a>Set conditions</a> to limit the promotion to specific orders.
+      <br />
+      Promotion applies only if all conditions are met.
+    </Text>
+  ),
+  onClick: () => {
+    alert('main clicked')
+  }
+}
+
+export const JustText = Template.bind({})
+JustText.args = {
+  fullWidth: true,
+  iconLabel: 'Add items',
+  onClick: () => {
+    alert('main clicked')
+  }
+}
