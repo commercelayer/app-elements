e010d559e6e593d126e5668f7f04cae92ac15826
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-01-15 23:24:25 +0100

    feat: add ButtonImageSelect component

---
+++ packages/docs/src/stories/atoms/ButtonImageSelect.stories.tsx
@@ -0,0 +1,26 @@
+import { ButtonImageSelect } from '#ui/atoms/ButtonImageSelect'
+import type { Meta, StoryFn } from '@storybook/react'
+
+const setup: Meta<typeof ButtonImageSelect> = {
+  title: 'Atoms/ButtonImageSelect',
+  parameters: {
+    layout: 'padded'
+  },
+  component: ButtonImageSelect
+}
+
+export default setup
+
+const Template: StoryFn<typeof ButtonImageSelect> = (args) => (
+  <div>
+    <ButtonImageSelect {...args} />
+  </div>
+)
+
+export const Primary = Template.bind({})
+Primary.args = {}
+
+export const WithImage = Template.bind({})
+WithImage.args = {
+  src: 'https://res.cloudinary.com/commercelayer/image/upload/f_auto,b_white/demo-store/skus/BASEBHAT000000FFFFFFXXXX_FLAT.png'
+}
