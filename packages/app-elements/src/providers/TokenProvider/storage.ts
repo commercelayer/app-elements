7b17b74681801f47bd08427dc0632272b76206da
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2024-02-02 12:16:29 +0100

    fix: allow TokenProvider to persist token using custom organizationSlug, it not provided infer org slug from URL

--- packages/app-elements/src/providers/TokenProvider/storage.ts
+++ packages/app-elements/src/providers/TokenProvider/storage.ts
@@ -1,46 +1,63 @@
 import { type TokenProviderAllowedApp } from './types'
 import { getOrgSlugFromCurrentUrl } from './url'
 
-type PersistentItem = 'accessToken'
-
 export function makeStorageKey({
   appSlug,
-  item
+  organizationSlug
 }: {
   appSlug: TokenProviderAllowedApp
-  item: PersistentItem
+  organizationSlug: string
 }): string {
-  return `${appSlug}:${getOrgSlugFromCurrentUrl()}:${item}`
+  return `${appSlug}:${
+    organizationSlug ?? getOrgSlugFromCurrentUrl()
+  }:accessToken`
 }
 
 export function getPersistentAccessToken({
-  appSlug
+  appSlug,
+  organizationSlug
 }: {
+  /** The app for which to get the token. */
   appSlug: TokenProviderAllowedApp
+  /** Try to infer it from the current URL when not explicitly provided. */
+  organizationSlug?: string
 }): string | null {
   if (typeof window === 'undefined') {
     return null
   }
 
   const storedAccessToken = window.localStorage.getItem(
-    makeStorageKey({ appSlug, item: 'accessToken' })
+    makeStorageKey({
+      appSlug,
+      organizationSlug:
+        organizationSlug ?? getOrgSlugFromCurrentUrl() ?? 'commercelayer'
+    })
   )
   return storedAccessToken
 }
 
 export function savePersistentAccessToken({
   appSlug,
-  accessToken
+  accessToken,
+  organizationSlug
 }: {
+  /** The app for which to get the token. */
   appSlug: TokenProviderAllowedApp
+  /** The token to save. */
   accessToken: string
+  /** Try to infer it from the current URL when not explicitly provided. */
+  organizationSlug?: string
 }): void {
   if (typeof window === 'undefined') {
     return
   }
 
   window.localStorage.setItem(
-    makeStorageKey({ appSlug, item: 'accessToken' }),
+    makeStorageKey({
+      appSlug,
+      organizationSlug:
+        organizationSlug ?? getOrgSlugFromCurrentUrl() ?? 'commercelayer'
+    }),
     accessToken
   )
 }
