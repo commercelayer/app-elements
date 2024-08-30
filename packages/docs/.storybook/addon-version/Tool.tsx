0b163802b3098998dbe572fcdec5ca0324210841
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-08-30 09:23:41 +0200

    docs: add app-elements version to the docs

---
+++ packages/docs/.storybook/addon-version/Tool.tsx
@@ -0,0 +1,23 @@
+import { IconButton, Separator } from '@storybook/components'
+import React from 'react'
+import { ADDON_NAME, LINK_URL, TOOL_ID, VERSION } from './constants'
+
+
+
+export const Tool = () => {
+  return (
+    <>
+      <Separator />
+      <IconButton
+        key={TOOL_ID}
+        title={ADDON_NAME}
+        active={false}
+        onClick={() => {
+          window.open(LINK_URL, '_blank')
+        }}
+      >
+        v{VERSION}
+      </IconButton>
+    </>
+  )
+}
