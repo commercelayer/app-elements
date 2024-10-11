df89b234d792f3b1cfedd0fbcea4e476cccfa9e2
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-10-11 15:53:53 +0200

    chore: update CodeEditor story

--- packages/docs/src/stories/forms/ui/CodeEditor.stories.tsx
+++ packages/docs/src/stories/forms/ui/CodeEditor.stories.tsx
@@ -113,23 +113,23 @@ PriceListRules.args = {
   ).concat('\n')
 }
 
-export const OrganizationConfig = Template.bind({})
-OrganizationConfig.args = {
-  name: 'organization-config',
-  label: 'Configuration',
-  height: '600px',
-  onChange: (value) => {
-    console.log('organization config • Changed to:', value)
-  },
-  onValidate: (markers) => {
-    console.log('organization config • Markers:', markers)
-  },
-  language: 'json',
-  jsonSchema: 'organization-config',
-  defaultValue: JSON.stringify({ mfe: { default: {} } }, undefined, 2).concat(
-    '\n'
-  )
-}
+// export const OrganizationConfig = Template.bind({})
+// OrganizationConfig.args = {
+//   name: 'organization-config',
+//   label: 'Configuration',
+//   height: '600px',
+//   onChange: (value) => {
+//     console.log('organization config • Changed to:', value)
+//   },
+//   onValidate: (markers) => {
+//     console.log('organization config • Markers:', markers)
+//   },
+//   language: 'json',
+//   jsonSchema: 'organization-config',
+//   defaultValue: JSON.stringify({ mfe: { default: {} } }, undefined, 2).concat(
+//     '\n'
+//   )
+// }
 
 export const WithHint = Template.bind({})
 WithHint.args = {
