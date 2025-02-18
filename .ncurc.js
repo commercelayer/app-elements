ca6b7d920c69d38b59ab4702fee1fe79175d8b8b
Marco Montalbano <marco.montalbano@commercelayer.io>
2025-02-18 18:18:52 +0100

    deps: temporary lock tailwindcss to ^3

--- .ncurc.js
+++ .ncurc.js
@@ -5,7 +5,8 @@ module.exports = {
   filterResults: (name, { upgradedVersionSemver }) => {
     if (
       name === '@types/node' && parseInt(upgradedVersionSemver?.major) >= 22 ||
-      name === 'eslint' && parseInt(upgradedVersionSemver?.major) >= 9
+      name === 'eslint' && parseInt(upgradedVersionSemver?.major) >= 9 ||
+      name === 'tailwindcss' && parseInt(upgradedVersionSemver?.major) >= 4
     ) {
       return false
     }
