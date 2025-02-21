f8bcdac573e6052aa9bc2544289008078c7ecca7
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2025-02-21 14:57:00 +0100

    feat: add ResourceAttachments component

---
+++ packages/docs/src/stories/resources/ResourceAttachments.stories.tsx
@@ -0,0 +1,30 @@
+import { CoreSdkProvider } from '#providers/CoreSdkProvider'
+import { MockTokenProvider as TokenProvider } from '#providers/TokenProvider/MockTokenProvider'
+import { ResourceAttachments } from '#ui/resources/ResourceAttachments'
+import { type Meta, type StoryFn } from '@storybook/react'
+
+const setup: Meta<typeof ResourceAttachments> = {
+  title: 'Resources/ResourceAttachments',
+  component: ResourceAttachments,
+  parameters: {
+    layout: 'padded'
+  }
+}
+export default setup
+
+const Template: StoryFn<typeof ResourceAttachments> = (args) => {
+  return (
+    <TokenProvider kind='integration' appSlug='orders' devMode>
+      <CoreSdkProvider>
+        <ResourceAttachments {...args} />
+      </CoreSdkProvider>
+    </TokenProvider>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  isLoading: false,
+  resourceType: 'orders',
+  resourceId: 'NMWYhbGorj'
+}
